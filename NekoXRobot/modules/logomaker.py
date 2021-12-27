de17ec675864a293db68d9f017a269fe85786f99
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-12-28 02:50:35 +0530

    | Yūki • Black Knights Union Project | NekoRobot

--- NekoXRobot/modules/logomaker.py
+++ NekoXRobot/modules/logomaker.py
@@ -558,7 +558,7 @@ async def lego(event):
     x = (image_widthz-w)/2
     y = ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
-    fname = "Vegeta.png"
+    fname = "Neko.png"
     img.save(fname, "png")
     await telethn.send_file(event.chat_id, file=fname, caption = f"Made for {neko} by [Neko](t.me/NekoXRobot) 💜")         
     await pesan.delete()
