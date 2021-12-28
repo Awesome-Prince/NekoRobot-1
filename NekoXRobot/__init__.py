8f7eb2b757ea40aad749cb6ba0bd7eeefcf53e16
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-12-28 21:41:02 +0530

    | Yūki • Black Knights Union Project | NekoRobot

--- NekoXRobot/__init__.py
+++ NekoXRobot/__init__.py
@@ -277,4 +277,4 @@ from KURUMIBOT.modules.helper_funcs.handlers import (CustomCommandHandler,
 # make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
 tg.CommandHandler = CustomCommandHandler
-tg.MessageHandler = CustomMessageHandler
+tg.MessageHandler = CustomMessageHandle
